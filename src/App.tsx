ec47deb04c20466b23f60403f249a0c18d962d92
AnyumbaKE <esanyumba@gmail.com>
2024-11-03 13:59:05 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,17 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import { Home } from './components/Home';
+import { CountryDetails } from './components/CountryDetails';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" data-testid="app-components-test-id">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/details/:name" element={<CountryDetails />} />
+        <Route path="*" element={<p>404 Page Not Found</p>} />
+      </Routes>
 
     </div>
   );
